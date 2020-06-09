ba697b34fe3bbc3627c8cd2c7068ea5dff5860a7
scina <dudek.mail@gmail.com>
2020-06-09 14:11:38 +0200

    Initial commit

---
+++ src/main/java/pl/wroc/assa/MyAppAssaDemo/MyAppAssaDemoApplication.java
@@ -0,0 +1,13 @@
+package pl.wroc.assa.MyAppAssaDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppAssaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAppAssaDemoApplication.class, args);
+	}
+
+}
